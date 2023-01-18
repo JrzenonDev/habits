1aab2d39618596d72bc12b9690413f74c33f1b39
JrzenonDev <jrobertoonb@gmail.com>
2023-01-18 00:10:47 -0300

    feat: configure fastify

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,1 +1,17 @@
-console.log('oi ts')
+import { PrismaClient } from '@prisma/client';
+import Fastify from 'fastify';
+
+const app = Fastify();
+const prisma = new PrismaClient();
+
+app.get('/', async () => {
+  const habits = await prisma.habit.findMany()
+
+  return habits
+})
+
+app.listen({
+  port: 3333,
+}).then(() => {
+  console.log('http server runing')
+})
